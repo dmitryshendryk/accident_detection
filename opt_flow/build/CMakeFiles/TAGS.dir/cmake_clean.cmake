b74fbba2c6542fc212544def0512744cd4272f2c
Dmitry <zaonmax@inbox.ru>
2019-04-20 15:57:29 +0800

    refactor

--- opt_flow/build/CMakeFiles/TAGS.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "compute_flow_automoc.cpp"
-  "CMakeFiles/TAGS"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TAGS.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
