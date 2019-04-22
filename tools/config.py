7d12ab6785d53551330524683aba530a88bd6122
Dmitry <zaonmax@inbox.ru>
2019-04-22 11:36:25 +0800

    add config

---
+++ tools/config.py
@@ -0,0 +1,6 @@
+class Config():
+
+    POST_URL = 'http://ec2-18-217-76-76.us-east-2.compute.amazonaws.com/api/v1/ai/message'
+    VIDEO_STREAM_PROD = 'rtsp://admin:12345abc@92.14.11.106:554/Streaming/Channels/1'
+    VIDEO_STREAM_TEST_LOCAL = 'dataset/videos/YoutubeVid2.mp4'
+    
