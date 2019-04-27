72e0ac2f99334876ca3c42231b71b532ca34881c
Dmitry <zaonmax@inbox.ru>
2019-04-27 19:35:19 +0800

    update

--- tools/rest_api.py
+++ tools/rest_api.py
@@ -6,6 +6,9 @@ from tools.config import Config
 
 import datetime
 
+from bson import json_util
+import json
+
 ROOT_DIR = os.path.abspath('../')
 sys.path.append(ROOT_DIR)
 
@@ -18,7 +21,7 @@ class RestAPI():
 
     def send_post(self):
         headers = {}
-        payload = {"cameraId": "1476320433439","type": "Danger","message": "Accident detected","cameraViewImageUrl": "link_to_detected_image", "eventTime": datetime.datetime.now()}
+        payload = {"cameraId": "1476320433439","type": "Danger","message": "Accident detected","cameraViewImageUrl": "link_to_detected_image", "eventTime": str(datetime.datetime.now())}
         r = requests.post(self.config.POST_URL, json=(payload), headers=headers)
         print("Response {}".format(r))
 
