aaa52c071b86bfce0949817df995c6694b6813a2
Dmitry <zaonmax@inbox.ru>
2019-04-27 17:22:33 +0800

    docker envs

--- tools/rest_api.py
+++ tools/rest_api.py
@@ -10,15 +10,10 @@ class RestAPI():
     def __init__(self):
         self.config = Config()
 
-
-
     def send_post(self, data):
-        headers = {'Content-type':'application/json',  
-                   'cache-control': 'no-cache'}
-        # payload = 
+        headers = {}
         payload = {"cameraId": "1476320433439","type": "Danger","message": "11 No helmet wearing rider","cameraViewImageUrl": "link_to_detected_image", "eventTime": "2019-04-23T18:25:43.511Z"}
-        r = requests.post(self.config.POST_URL, data={'test': 'cheers'}, headers=headers)
-        print(r)
+        r = requests.post(self.config.POST_URL, json=(payload), headers=headers)
 
 
 if __name__ =='__main__':
