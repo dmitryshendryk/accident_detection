9cc3dc738801459aab7ae613b742d3f25ee82cfa
Dmitry <zaonmax@inbox.ru>
2019-04-27 12:44:11 +0800

    hotfix

--- tools/rest_api.py
+++ tools/rest_api.py
@@ -3,6 +3,7 @@ import os
 import requests
 from config import Config
 
+import json 
 
 class RestAPI():
 
@@ -12,7 +13,14 @@ class RestAPI():
 
 
     def send_post(self, data):
-        headers = {'Authorization': 'Bearer ' + self.config.TOKEN}
-        payload = data 
+        headers = {'Content-type':'application/json',  
+                   'cache-control': 'no-cache'}
+        # payload = 
+        payload = {"cameraId": "1476320433439","type": "Danger","message": "11 No helmet wearing rider","cameraViewImageUrl": "link_to_detected_image", "eventTime": "2019-04-23T18:25:43.511Z"}
+        r = requests.post(self.config.POST_URL, data={'test': 'cheers'}, headers=headers)
+        print(r)
 
-        r = requests.post(self.config.POST_URL, json=(payload), headers=headers)
+
+if __name__ =='__main__':
+    rest = RestAPI()
+    rest.send_post("test")
