cb101afe4993c300872eda170c80b10a5cbf7237
Dmitry <zaonmax@inbox.ru>
2019-04-29 13:29:05 +0800

    online camera handler

--- workspace/main.py
+++ workspace/main.py
@@ -618,6 +618,7 @@ def detection(model, image_path=None, video_path=None, camera_info=None):
             ref, image = cap.read()
             if ref is None:
                 continue
+            print(len(image))
             r = model.detect([image], verbose=1)[0]
 
             if (len(r['rois']) != 0):
