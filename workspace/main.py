63d53140f9345c1336e362c7cf27b75d8c91433e
Dmitry <zaonmax@inbox.ru>
2019-04-30 20:44:34 +0800

    add time measure

--- workspace/main.py
+++ workspace/main.py
@@ -51,8 +51,12 @@ class CarPlateConfig(Config):
 class InferenceConfig(CarPlateConfig):
             # Set batch size to 1 since we'll be running inference on
             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    GPU_COUNT = 2
+    GPU_COUNT = 1
     IMAGES_PER_GPU = 2
+
+    IMAGE_MIN_DIM = int(256)
+    IMAGE_MAX_DIM = int(640)
+    POST_NMS_ROIS_INFERENCE = 300
 
 
 class CarPlateDataset(utils.Dataset):
