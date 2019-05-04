420de2ada456fe773d8fe17a0d69d5fce428e428
Dmitry <zaonmax@inbox.ru>
2019-05-04 18:19:15 +0800

    update yolo

--- yolo.py
+++ yolo.py
@@ -18,11 +18,13 @@ from PIL import Image, ImageFont, ImageDraw
 from yolo3.model import yolo_eval
 from yolo3.utils import letterbox_image
 
+ROOT_DIR = os.path.abspath('./')
+
 class YOLO(object):
     def __init__(self):
-        self.model_path = 'model_data/yolo.h5'
-        self.anchors_path = 'model_data/yolo_anchors.txt'
-        self.classes_path = 'model_data/coco_classes.txt'
+        self.model_path = ROOT_DIR + '/model_data/yolo.h5'
+        self.anchors_path = ROOT_DIR + '/model_data/yolo_anchors.txt'
+        self.classes_path = ROOT_DIR + '/model_data/coco_classes.txt'
         self.score = 0.5
         self.iou = 0.5
         self.class_names = self._get_class()
