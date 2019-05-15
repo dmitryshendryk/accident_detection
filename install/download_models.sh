ac6b4b3abbd4cbab9cf47f44f7025d6e96b19e7d
Dmitry <zaonmax@inbox.ru>
2019-05-15 12:45:53 +0800

    add model

--- install/download_models.sh
+++ install/download_models.sh
@@ -47,5 +47,12 @@ else
     wget http://ec2-18-217-76-76.us-east-2.compute.amazonaws.com:8090/file/accident_detection/video_1_LSTM_1_1024.h5
 fi
 
+if [ -f vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5 ]
+then 
+    echo "File already downloaded, skipping"
+else
+    wget https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5
+fi
+
 
 cd ..
--- workspace/main.py
+++ workspace/main.py
@@ -830,7 +830,7 @@ def train_model(model, subfolder, mode_train):
                     layers='all')
 
 def load_VGG16_model():
-  base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
+  base_model = VGG16(weights=ROOT_DIR + '/model_data/' + 'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5', include_top=False, input_shape=(224,224,3))
   print ("Model loaded..!")
   print (base_model.summary())
   return base_model
