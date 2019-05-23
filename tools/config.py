3f3a624231a5e0768f3f219ec52a75217e9dea0f
Alina <erar1@mail.ru>
2019-05-23 23:53:30 +0300

    change db and post address

--- tools/config.py
+++ tools/config.py
@@ -1,9 +1,9 @@
 class Config():
 
-    POST_URL = 'http://ec2-18-217-76-76.us-east-2.compute.amazonaws.com/api/v1/ai/message'
+    POST_URL = 'http://ec2-18-223-248-81.us-east-2.compute.amazonaws.com//api/v1/ai/message'
     TOKEN = '4cb3d1e9-f6c3-4d47-9ef2-c65b394ac8d7'
 
-    server = 'ec2-18-217-76-76.us-east-2.compute.amazonaws.com,5434'
+    server = 'ec2-18-223-248-81.us-east-2.compute.amazonaws.com,5434'
     database = 'master'
     username = 'AIUSER'
     password = 'AIPassword!q@'
