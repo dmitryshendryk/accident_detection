44042dffcc05a293576f15ef2558ab2583321b1c
Alina <erar1@mail.ru>
2019-07-10 23:57:32 +0300

    config change for env

--- tools/config.py
+++ tools/config.py
@@ -1,8 +1,12 @@
+import os
 class Config():
+    SERVER_URL = "rtmp://" + os.environ['SERVER_URL']
+    IMAGE_PORT = os.environ['IMAGE_PORT']
+    STREAM_PORT = os.environ['STREAM_PORT']
 
     POST_URL = 'http://ec2-3-17-203-43.us-east-2.compute.amazonaws.com:8080/api/v1/ai/message'
     TOKEN = '4cb3d1e9-f6c3-4d47-9ef2-c65b394ac8d7'
-    IMG_URL_REMOTE = 'http://35.193.146.105:8090'
+    IMG_URL_REMOTE = "http://" + os.environ['SERVER_URL'] + ":" + IMAGE_PORT
     IMG_URL_LOCAL = 'http://localhost:8090'
 
     server = 'ec2-18-223-248-81.us-east-2.compute.amazonaws.com,5434'
@@ -14,6 +18,3 @@ class Config():
 
     camera_table = 'TectumTraffic.ai.Camera'
     camera_server_table = 'TectumTraffic.ai.ServerCamera'
-
-    SERVER_URL = 'rtmp://35.193.146.105'
-    STREAM_PORT = '1935'    
