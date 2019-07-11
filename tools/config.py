0784420c0704a93fd053903e16f6812d8298249d
Dmitry <zaonmax@inbox.ru>
2019-07-11 22:20:58 +0800

    change ip

--- tools/config.py
+++ tools/config.py
@@ -2,7 +2,7 @@ class Config():
 
     POST_URL = 'http://ec2-3-17-203-43.us-east-2.compute.amazonaws.com:8080/api/v1/ai/message'
     TOKEN = '4cb3d1e9-f6c3-4d47-9ef2-c65b394ac8d7'
-    IMG_URL_REMOTE = 'http://35.193.146.105:8090'
+    IMG_URL_REMOTE = 'http://35.232.194.111:8090'
     IMG_URL_LOCAL = 'http://localhost:8090'
 
     server = 'ec2-18-223-248-81.us-east-2.compute.amazonaws.com,5434'
@@ -15,5 +15,5 @@ class Config():
     camera_table = 'TectumTraffic.ai.Camera'
     camera_server_table = 'TectumTraffic.ai.ServerCamera'
 
-    SERVER_URL = 'rtmp://35.193.146.105'
+    SERVER_URL = 'rtmp://35.232.194.111'
     STREAM_PORT = '1935'    
