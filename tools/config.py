cca1815c94ece6ac942a00aacd0cc14ef4dac51c
Alina <erar1@mail.ru>
2019-08-04 18:14:56 +0300

    change host address

--- tools/config.py
+++ tools/config.py
@@ -5,7 +5,7 @@ class Config():
 
     POST_URL = 'http://ec2-3-17-203-43.us-east-2.compute.amazonaws.com:8080/api/v1/ai/message'
     TOKEN = '4cb3d1e9-f6c3-4d47-9ef2-c65b394ac8d7'
-    IMG_URL_REMOTE = 'http://35.232.194.111:8090'
+    IMG_URL_REMOTE = 'http://35.239.192.42:8090'
     IMG_URL_LOCAL = 'http://localhost:8090'
 
     server = 'ec2-18-223-248-81.us-east-2.compute.amazonaws.com,5434'
@@ -20,6 +20,6 @@ class Config():
 
     # SERVER_URL = 'rtmp://35.232.194.111'
     # STREAM_PORT = '1935'    
-    SERVER_URL = 'rtmp://' + os.getenv('SERVER_URL', '35.232.194.111')
+    SERVER_URL = 'rtmp://' + os.getenv('SERVER_URL', '35.239.192.42')
     STREAM_PORT = os.getenv('STREAM_PORT', '1935')
 
